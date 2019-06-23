b4b1581292554c66dd9c0f316ef797489a5fd4f4
Daisuke Tsuji <dim0627@gmail.com>
2019-06-23 18:23:09 +0900

    :ok_hand: thru jwt auth.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
+const jwt = require('jsonwebtoken')
 
 const { prisma } = require('./generated/prisma-client/index');
+
+const getUser = token => {
+  try {
+    if (token) {
+      return jwt.verify(token, 'my-secret-from-env-file-in-prod')
+    }
+    return null
+  } catch (err) {
+    return null
+  }
+}
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: {
-    prisma
-  }
+  context: ({ req }) => {
+    const tokenWithBearer = req.headers.authorization || ''
+    const token = tokenWithBearer.split(' ')[1]
+    console.log(token)
+    const user = getUser(token)
+
+    return {
+      user,
+      prisma, // the generated prisma client if you are using it
+    }
+  },
 });
 
 server
